9e70ed5690bd2f8adf003592e5e6c83a8ff0dec7
CarolineTomasik <Caroline.tomasik@gmail.com>
2020-06-01 10:58:53 -0700

    Update nlp_code.py
    clean, working code that lists all articles and word frequency

--- src/nlp_code.py
+++ src/nlp_code.py
@@ -27,60 +27,123 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 nltk.download('wordnet')
+from stanfordcorenlp import StanfordCoreNLP
+from Stanford_lemmatizer import lemmatize_corenlp
+from itertools import chain
 
-#download sqlite database
-try:
-    conn = sqlite3.connect('../data/articles.db')
-    print ("Opened database successfully");
-    
-except Exception as e:
-    print("Error during connection:",str(e))
-
-
-# separate fox text from cnn text 
+# select articles text by site
 def get_site_articles(site):
     cur = conn.cursor()
-    sql = "SELECT * FROM articles WHERE site = " + "'" + site + "';"
+    sql = "SELECT * FROM articles WHERE site = '{}' AND length(article) > 0;".format(site)
     cur.execute(sql)
     articles_list = []
-    #rows = cur.fetchmany(2)
+    #rows = cur.fetchmany(31)
     rows = cur.fetchall()
     for row in rows:
-        articles_list.append(row[4])
+        if len(row[4]) > 0:
+            articles_list.append(row[4])
     return articles_list
 
-fox_text = get_site_articles('www.foxnews.com') 
-#print(fox_text)  
-cnn_text = get_site_articles('www.cnn.com')
-#print(cnn_text)
 
-#clean text: tokenize, remove enters, remove punctuation, remove stop words, and lemmatize
-stop_words = set(stopwords.words('english'))
-print(stop_words)
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-print(lemmatizer)
-extra_stopwords = ['this', 'also', 'fox', 'cnn', 'click', 'caption']
+def configure_stopwords():
+	stop_words = set(stopwords.words('english'))
+	extra_stopwords = ['this', 'also', 'fox', 'cnn', 'click', 'caption']
+	for extra in extra_stopwords:
+		stop_words.add(extra)
+	return stop_words
 
-for extra in extra_stopwords:
-    stop_words.add(extra)
-    
-
-def cleaning_text(text):
+ 
+def cleaner(stop_words, text):
+	#clean text: tokenize, remove newlines, remove punctuation, remove stop words
     lowered = str(text).lower().replace('\\n\\n', ' ').replace("\\\'","")
-    lemmatized = lemmatizer.lemmatize(lowered)
-    tokens = [str(token) for token in nlp_en(lemmatized)]
+    tokens = [str(token) for token in nlp_en(lowered)]
     words = [word for word in tokens if word.isalpha()]
     words = [w for w in words if not w in stop_words]
     return words
-    
+
+
+def preprocess_text(site):
+		articles_list = get_site_articles(site) 
+		clean_list = []
+		for article in articles_list:
+			clean = cleaner(stop_words, article)
+			clean = lemmatize_corenlp(conn_nlp=nlp, sentence=' '.join(clean))
+			clean_list.append(clean)
+		return clean_list
+
+
+def main():
+	#connect to sqlite database
+	try:
+		conn = sqlite3.connect('../data/articles.db')
+		print ("Opened database successfully");
+	except Exception as e:
+		print("Error during connection:",str(e))
+
+	# connect to Standford lemmatizer
+	nlp = StanfordCoreNLP('http://localhost', port=9000, timeout=30000)
+
+	stop_words = configure_stopwords()
+
+	fox_clean = preprocess_text('www.foxnews.com')
+	cnn_clean = preprocess_text('www.cnn.com')
+
+	print("Found {0} FOXnews and {1} CNN articles".format(len(fox_clean), len(cnn_clean)))
+
+	print(Counter(list(chain(*fox_clean))).most_common(10))
+	print(Counter(list(chain(*cnn_clean))).most_common(10))
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+#Count frequencies and graph
+freq = nltk.FreqDist(clean_fox)
+#print(freq)
+for key,val in freq.items():
+    print(str(key) + ':' + str(val))    
+freq.plot(20, cumulative=False)    
+
+freq = nltk.FreqDist(clean_cnn)
+#print(freq)
+for key,val in freq.items():
+    print(str(key) + ':' + str(val))    
+freq.plot(20, cumulative=False) 
+
+
+
+
+
+
+def cleaning_text(text):
+    lowered = str(text).lower().replace('\\n\\n', ' ').replace("\\\'","")
+    #lemmatized = lemmatize_corenlp(conn_nlp=nlp, sentence=' '.join(lowered))
+    tokens = [str(token) for token in nlp_en(lowered)]
+    words = [word for word in tokens if word.isalpha()]
+    words = [w for w in words if not w in stop_words]
+    return words
+
+
+fox_text = ' '.join(lemmatizer(fox_text))    
+fox_clean = cleaning_text((lemmatize_corenlp(conn_nlp=nlp, sentence=fox_text)))
+
+cnn_text =  ' '.join(lemmatizer(cnn_text))    
+cnn_clean = cleaning_text((lemmatize_corenlp(conn_nlp=nlp, sentence=cnn_text)))
+print(cnn_clean)
+
+
 
 clean_fox = cleaning_text(fox_text)
-#print(clean_fox)
+print(clean_fox)
+
 clean_cnn = cleaning_text(cnn_text)
 #print(clean_cnn)
-clean_list = ['coronavirus', 'coronaviruses', 'is', 'was', 'be']
-print(clean_list)
+lemma_test = ['coronavirus', 'coronaviruses', 'is', 'was', 'be']
+#print(cleaning_text(lemma_test))
+lemma1 = lemmatize_corenlp(conn_nlp=nlp, sentence=' '.join(lemma_test))
+#print(lemma1)
 
 
 #POS tagging
@@ -92,7 +155,8 @@ fox_text_POS = preprocess(clean_fox)
 print(fox_text_POS)
 cnn_text_POS = preprocess(clean_cnn)
 print(cnn_text_POS)
-
+"""
+"""
 
 #entity tagging
 # import sys
@@ -119,16 +183,16 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
-def most_common(text):
-    article = nlp(str(text))
-    len(article.ents)
-    labels = [x.label_ for x in article.ents]
-    Counter(labels)
-    items = [x.text for x in article.ents]
-    return Counter(items).most_common(5)
+# def most_common(text):
+#     article = nlp(str(text))
+#     len(article.ents)
+#     labels = [x.label_ for x in article.ents]
+#     Counter(labels)
+#     items = [x.text for x in article.ents]
+#     return Counter(items).most_common(5)
 
-most_common(clean_fox)
-most_common(clean_cnn)
+# most_common(clean_fox)
+# most_common(clean_cnn)
 
 
 def highlighted_sents(text):
@@ -140,24 +204,60 @@ def highlighted_sents(text):
 highlighted_sents(clean_fox)
 highlighted_sents(clean_cnn)
 
-#Count frequencies and graph
-freq = nltk.FreqDist(clean_fox)
-#print(freq)
-for key,val in freq.items():
-    print(str(key) + ':' + str(val))    
-freq.plot(20, cumulative=False)    
+Counter(clean_fox).most_common(5)
+Counter(clean_cnn).most_common(5)
 
-freq = nltk.FreqDist(clean_cnn)
-#print(freq)
-for key,val in freq.items():
-    print(str(key) + ':' + str(val))    
-freq.plot(20, cumulative=False) 
+#classification
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+fox_df = pd.DataFrame(clean_fox)
+fox_df
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(clean_fox).toarray()
+y = clean_fox.iloc[:, 1].values
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
 #ATTEMPTS
@@ -194,15 +294,15 @@ print(X.shape)
 
 
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = TfidfVectorizer()  
-analyze = vectorizer.build_analyzer() 
-print(‘Fox’,analyze(fox_text))
-print(‘CNN’,analyze(cnn_text))
-print(‘Document transform’,X.toarray())      
+# from sklearn.feature_extraction.text import TfidfVectorizer
+# vectorizer = TfidfVectorizer()  
+# analyze = vectorizer.build_analyzer() 
+# print(‘Fox’,analyze(fox_text))
+# print(‘CNN’,analyze(cnn_text))
+# print(‘Document transform’,X.toarray())      
 
-X = vectorizer.fit_transform(fox_text)
-print(vectorizer.get_feature_names()) 
+# X = vectorizer.fit_transform(fox_text)
+# print(vectorizer.get_feature_names()) 
 
 
 
@@ -311,3 +411,5 @@ sortFreqDict(clean_fox)
 
 
 Counter(clean_fox)
+'''
+
